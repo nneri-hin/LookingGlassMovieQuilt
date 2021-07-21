0840a387922e57d2cdc507b37a9f7f83db6ee8a4
junk1ekuni@gmail.com <thamurian@gmail.com>
2021-07-21 10:21:24 +0900

    一応動くはず
    スマホで縦で撮った動画は39行目をコメントアウトすると多分動く

--- create-quilt.py
+++ create-quilt.py
@@ -9,10 +9,15 @@ import cv2
 
 W= 8
 H= 6
+#W=5
+#H=9
+
 MULTI_W = 3840
 MULTI_H = 3840
-SINGLE_W = int(3840 / W)
-SINGLE_H = int(3840 / H)
+MULTI_W = 7680
+MULTI_H = 7680
+SINGLE_W = int(MULTI_W / W)
+SINGLE_H = int(MULTI_H / H)
 
 #一枚画像
 def createBuf():
@@ -31,20 +36,31 @@ if __name__ == '__main__':
         if frame is None:
             break
         #とりあえず全部90度回転させる方針
-        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        #frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        #frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
         worg = frame.shape[1]
         horg = frame.shape[0]
         w = int(frame.shape[0] * aspect)
         h = int(frame.shape[1] / aspect)
-        if frame.shape[0] > frame.shape[1] : 
+        print(w,h,aspect)
+        movAspect = frame.shape[0] / frame.shape[1]
+        print(aspect,movAspect,frame.shape[0],frame.shape[1])
+        print(movAspect / aspect)
+        print( aspect / movAspect)
+        #if frame.shape[0] > frame.shape[1] : 
+        if movAspect / aspect > 1 :
+            print("trim1")
             #trim = frame[:, int((worg - w) / 2 )  : int(  ( worg+w) / 2  ) ]
             trim = frame[ int((horg - h) / 2 )  : int(  ( horg+h) / 2  ),: ] 
+            print(trim.shape)
             pass
         else:
+            print("trim2")
             trim = frame[:, int((worg - w) / 2 )  : int(  ( worg+w) / 2  ) ]
+            print(frame.shape)
+            print(trim.shape)
         print(trim.shape)
         resized = cv2.resize(trim,(SINGLE_W,SINGLE_H))
-        print(resized.shape)
         images.append(resized)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -55,18 +71,21 @@ if __name__ == '__main__':
     pos = 0.0
     p = 0
     final = np.zeros((MULTI_W,MULTI_H,3),np.uint8)
-    while pos < cnt - 1 :
+
+    while (pos < cnt - 1)  and p < W*H:
         pos = pos + offset
         i = round(pos)
         ws = p % W * SINGLE_W
         hs = int(p / W) *SINGLE_H
-
-        final[hs:hs+SINGLE_H,ws:ws+SINGLE_W]= images[i]
-
+        if i >len(images) -1 :
+            i = len(images) -1 
+        #final[hs:hs+SINGLE_H,ws:ws+SINGLE_W]= images[i]
+        final[hs:hs+SINGLE_H,MULTI_W-ws-SINGLE_W:MULTI_W-ws]= images[i]
         #hs = p % H * SINGLE_H
         #ws = int(p/H) * SINGLE_W
         #final[ws:ws+SINGLE_W,hs:hs+SINGLE_H] = images[i]
         p += 1
     cap.release()
-    cv2.imwrite("out.png",final)
+    cv2.imwrite(a[2],final)
     #cv2.destroyAllWindows()
+
